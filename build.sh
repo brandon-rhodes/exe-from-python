34d68483f6c61b5547762e8c38b9921372c42d5f
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-01 23:24:51 -0400

    Isolate Docker-necessary files in a subdirectory
    Because https://github.com/dotcloud/docker/issues/2224

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
+mkdir -p output/cython output/nuitka
+
 for script in $(ls scripts | sed '/\.py$/s/\.py//')
 do
     (cd cython && make TARGET=$script)
+    cp cython/$script output/cython/$script
     (cd nuitka && make TARGET=$script)
+    cp nuitka/$script.dist/$script.exe output/nuitka/$script
 done
---
+++ output/compare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+for script in $(ls ../scripts | sed '/\.py$/s/\.py//')
+do
+    echo ========= $script =========
+    echo
+    ./cython/$script
+    echo
+    ./nuitka/$script
+    echo
+done
