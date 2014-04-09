9dc7fea76b6105e3ff95869a034103001b91a408
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-09 10:36:16 -0400

    For comparison, do harmonic sum with NumPy

--- scripts/harmonic_sum.py
+++ scripts/harmonic_sum.py
@@ -1,8 +1,8 @@
-from sys import version_info
+import sys
 from time import time
 
 ten_million = 10000000
-xrange = range if version_info >= (3, 0) else xrange
+xrange = range if sys.version_info >= (3, 0) else xrange
 
 def compute_harmonic_sum(terms=ten_million):
     t0 = time()
@@ -13,5 +13,17 @@ def compute_harmonic_sum(terms=ten_million):
     print('Sum {}'.format(n))
     print('Milliseconds {}'.format((t1 - t0) * 1000.0))
 
+def compute_harmonic_sum_with_numpy(terms=ten_million):
+    import numpy as np
+    t0 = time()
+    v = np.arange(1.0, terms + 1.0, 1.0)
+    n = (1.0 / v).sum()
+    t1 = time()
+    print('Sum {}'.format(n))
+    print('Milliseconds {}'.format((t1 - t0) * 1000.0))
+
 if __name__ == '__main__':
-    compute_harmonic_sum()
+    if len(sys.argv) > 1:
+        compute_harmonic_sum_with_numpy(terms=ten_million)
+    else:
+        compute_harmonic_sum()
